cf2ceee2bac71d6ff94ec58e01cea02cd2a2c38b
seireitou <ausis@mail.com>
2021-04-29 16:04:13 -0500

    commit tree 399f9b9f727e325ae019d2a626120830b73f5db8

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+function saturdayFun(actvity = "roller-skate") {
+    return `This Saturday, I want to ${actvity}!`;
+}
+
+function mondayWork(actvity = 'go to the office') {
+    return `This Monday, I will ${actvity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(e = 'special') {
+        console.log(`You are ${flair}${e}${flair}`)
+        return `You are ${flair}${e}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y,
+}
+function actionApplyer(int, functs) {
+    if (functs.length === 0) {
+        return int
+    } else {
+        let result = int
+        functs.forEach(funct => {
+            result = funct(result)
+         });
+        return result
+        }
+    }
+// wrapAdjective()
